6d7b35b1f6dc72b88903b5ecd26fed041fed0ac6
VLADISLAV STAVTSEV <140553104+VladislavStavtsev@users.noreply.github.com>
2026-03-04 04:39:59 +0300

    Add files via upload

---
+++ lab8/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'f1230120_login_db');
+define('DB_USER', 'f1230120_login_db');
+define('DB_PASSWORD', 'mPc-F8v-P6U-FyQ');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+?>
