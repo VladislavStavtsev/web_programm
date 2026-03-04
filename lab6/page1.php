8bbb2363af3e476a3545ddb1ed1f0d8407f0b0b8
VLADISLAV STAVTSEV <140553104+VladislavStavtsev@users.noreply.github.com>
2026-03-04 04:38:14 +0300

    Add files via upload

---
+++ lab6/page1.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+include('savepage.inc.php');
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Страница 1</title>
+</head>
+<body>
+    <h1>Страница 1</h1>
+    <?php include('menu.inc.php'); ?>
+    <?php include('visited.inc.php'); ?>
+</body>
+</html>
---
+++ lab6/savepage.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$currentPage = $_SERVER['PHP_SELF'];
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['visited_pages'])) {
+    $_SESSION['visited_pages'] = []; 
+}
+
+if (empty($_SESSION['visited_pages']) || end($_SESSION['visited_pages']) !== $currentPage) {
+    $_SESSION['visited_pages'][] = $currentPage;
+}
+?>
